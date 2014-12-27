9e8b47cd96a5370ad2ac1e70b90f430cb413fc78
KillermenBE <davy.de.coster@gmail.com>
2014-12-27 14:41:08 +0100

    User Management System added

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,26 @@ var Account = require('../data/models/account');
 var comm = require('../app/comm');
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user });
+    if(req.session['user']!= null)
+    {
+        return res.render('index', { user : req.session['user'], pagetitle : "RoboDog | Home" });
+    }
+    res.render('index', {pagetitle : "RoboDog | Home" });
+
 });
 
+//USER SYSTEM ROUTES
+
 router.get('/register', function(req, res) {
-    res.render('register', { });
+    if(req.session['user']!=null)
+    {
+        return res.redirect('/');
+    }
+    res.render('register', {pagetitle : "RoboDog | Registration" });
 });
 
 router.post('/register', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    Account.register(new Account({ username : req.body.username, admin:false }), req.body.password, function(err) {
         if (err) {
             return res.render("register", {info: "Sorry. That username already exists. Try again."});
         }
@@ -25,11 +36,11 @@ router.post('/register', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+    res.render('login', { user : req.user, pagetitle : "RoboDog | Login" });
 });
 
 router.post('/login', function(req,res,next){
-    passport.authenticate('local', function(err, user,info){
+    passport.authenticate('local', function(err, user){
         if (err) {
             return next(err); // will generate a 500 error
         }
@@ -37,19 +48,25 @@ router.post('/login', function(req,res,next){
         if (! user) {
             return res.render('login',{info:"Login not found"});
         }
-        return res.redirect('/control');
+        req.session['user']=user;
+        return res.redirect('/');
     })(req, res, next);
 });
 
 router.get('/logout', function(req, res) {
     req.logout();
+    req.session['user']=null;
     res.redirect('/');
 });
 
 //CONTROL CENTER ROUTES
 
 router.get('/control', function(req,res){
-    res.render('control', { user: req.user });
+    if(req.session['user']!= null)
+    {
+        return res.render('control', { user: req.session['user'], pagetitle : "RoboDog | Control Center" });
+    }
+    res.redirect('/login');
 });
 
 router.get('/control/forward',function(req,res){
@@ -72,4 +89,40 @@ router.get('/control/stop',function(req,res){
     res.send('ok');
 });
 
+//ADMIN CENTER
+
+router.get('/admin', function(req,res){
+    {
+        if (req.session['user'] == null) {
+            return res.redirect('/login');
+        }
+        if(req.session['user'].admin == false)
+        {
+            return res.redirect('/control');
+        }
+        Account.find({}).exec(function (err, docs) {
+            res.render('admin', {users:docs, pagetitle : "RoboDog | Administrator Center" });
+        });
+    }
+});
+
+router.get('/admin/edit/:id', function(req,res){
+    Account.findById(req.param('id')).exec(function (err, docs){
+        res.render('edit', {user:docs});
+    });
+});
+
+router.post('/admin/edit', function (req, res){
+    Account.findByIdAndUpdate(req.param('id'),{username:req.param('username'), admin:req.param('admin')}).exec(function(){
+        req.session['user'].admin=req.param('admin');
+        req.session['user'].username = req.param('username');
+        res.redirect('/admin');
+    });
+});
+
+router.get('/admin/delete/:id',function(req,res){
+    Account.findByIdAndRemove(req.param('id')).exec(function(){
+        res.redirect('/admin');
+    });
+});
 module.exports = router;
